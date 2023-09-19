f696b093c8f9edbcaef6abb937bb5d70c4e57a57
Adel Fattakhova <adel.fattakhova.162@gmail.com>
2023-09-19 16:44:22 +0300

    [PT-16] feat: use vite-svg-loader to render svg icons

---
+++ src/components/BaseIcon.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { defineAsyncComponent } from 'vue'
+
+const props = defineProps<{
+	name: string
+	width?: number
+	height?: number
+}>()
+
+const icon = defineAsyncComponent(() => import(`../assets/svg/${props.name}.svg`))
+</script>
+
+<template>
+	<component :is="icon" style="width: width; height: height" />
+</template>
