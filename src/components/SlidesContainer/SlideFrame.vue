520c30ba9957e2210a2c33cb26f7bbbb388bcdd9
pmorozova <Lermenua@yandex.ru>
2023-10-01 22:07:33 +0300

    [PT-27] feat: remove slide

--- src/components/SlidesContainer/SlideFrame.vue
+++ src/components/SlidesContainer/SlideFrame.vue
@@ -1,18 +1,34 @@
-<script setup lang="ts">
-defineProps<{
-	slideNumber: number
-	selected: boolean
-}>()
-</script>
-
 <template>
 	<div :class="[$style.container]">
 		<div :class="[$style.slideNumber]">{{ slideNumber }}</div>
-		<div :class="[$style.miniature, selected ? $style.selected : '']"></div>
+		<div :class="[$style.miniature, selected ? $style.selected : '']">
+			<button :class="[$style.btnRemove]" @click="remove">Remove</button>
+		</div>
 	</div>
 </template>
 
-<style module>
+<script setup lang="ts">
+
+type SlideItemProps = 	{
+	slideNumber: number,
+	slideIndex: string,
+	selected: boolean,
+}
+
+type SlideItemEmits = {
+	remove: [id: string]
+}
+
+const props = defineProps<SlideItemProps>();
+const emits = defineEmits<SlideItemEmits>();
+
+const remove = () => {
+	emits('remove', props.slideIndex);
+}
+
+</script>
+
+<style module scoped>
 .container {
 	display: flex;
 	justify-content: end;
@@ -27,10 +43,18 @@ defineProps<{
 .miniature {
 	width: 157px;
 	height: 92px;
+	display: flex;
+	justify-content: flex-end;
 
 	margin-left: 8px;
 	border: 1px solid var(--pt-light-grey);
 	border-radius: var(--pt-border-radius);
+}
+
+.btnRemove {
+	background-color: aqua;
+	width: 34px;
+	height: 34px;
 }
 
 .selected {
--- src/components/SlidesContainer/SlidesContainer.vue
+++ src/components/SlidesContainer/SlidesContainer.vue
@@ -1,12 +1,11 @@
 <script setup lang="ts">
-import SlideFrame from '@/components/SlidesContainer/SlideFrame.vue'
 import { ref } from 'vue'
-import type { Ref } from 'vue'
-import type { SlideData } from '@/components/SlidesContainer/types'
+import SlideFrame from '@/components/SlidesContainer/SlideFrame.vue'
 import { generateId } from '@/components/SlidesContainer/helpers'
 import BaseButton from '@/components/buttons/BaseButton.vue'
+import type { SlideData } from '@/components/SlidesContainer/types'
 
-const slides: Ref<SlideData[]> = ref([
+const slides = ref<SlideData[]>([
 	{ id: '1' },
 	{ id: '2' },
 	{ id: '3' },
@@ -14,7 +13,7 @@ const slides: Ref<SlideData[]> = ref([
 	{ id: '5' },
 	{ id: '6' },
 ])
-const selectedSlide = ref<string>('1')
+const selectedSlide = ref('1')
 
 function selectSlide(slideNumber: string) {
 	selectedSlide.value = slideNumber
@@ -23,21 +22,28 @@ function selectSlide(slideNumber: string) {
 function addSlide() {
 	slides.value.push({ id: generateId() })
 }
+
+const removeSlide = (id: string) => {
+	const slideIndex = slides.value.findIndex(item => item.id === id);
+	slides.value.splice(slideIndex, 1);
+}
+
 </script>
 
 <template>
 	<div :class="$style.container">
-		<BaseButton icon-name="plus" :action="addSlide" variant="light" :class="[$style.button]"
-			>Добавить слайд</BaseButton
-		>
+		<BaseButton icon-name="plus" :action="addSlide" variant="light" :class="[$style.button]">
+			Добавить слайд</BaseButton>
 		<div :class="$style.slidesContainer">
 			<SlideFrame
 				v-for="(slide, index) in slides"
 				:class="[$style.slideItem]"
 				:key="slide.id"
-				:slideNumber="index + 1"
+				:slide-index="slide.id"
+				:slideNumber="index+1"
 				:selected="selectedSlide === slide.id"
 				@click="selectSlide(slide.id)"
+				@remove="removeSlide"
 			/>
 		</div>
 	</div>
