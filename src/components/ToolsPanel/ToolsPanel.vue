fd0f1fffb69111efb7e47191f36d4a04c2d9daaf
Nik <playpaper12@gmail.com>
2023-10-25 17:44:47 +0300

    [PT-20] feat: added ToolsPanel component

---
+++ src/components/ToolsPanel/ToolsPanel.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import ToolsButton from '@/components/ToolsPanel/ToolsButton.vue'
+import { History } from '@/main'
+</script>
+
+<template>
+	<div :class="$style.toolsPanel">
+		<ToolsButton icon-name="arrow-left" :action="() => History.undo()" />
+		<ToolsButton icon-name="arrow-right" :action="() => History.redo()" />
+	</div>
+</template>
+
+<style module>
+.toolsPanel {
+	display: flex;
+	align-items: center;
+	height: 32px;
+	padding-left: 4px;
+	background-color: var(--pt-lighter-grey);
+	border-radius: var(--pt-border-radius);
+}
+</style>
