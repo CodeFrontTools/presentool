37d32bdf45ca9d25ad4f7570e6cef470c5d34bae
Nik <playpaper12@gmail.com>
2023-10-28 06:47:25 +0300

    fix: init first slide

--- src/components/constants.ts
+++ src/components/constants.ts
@@ -1,3 +1,7 @@
+import { Slide } from '@/types'
+
 export const FONT_SIZE = 18
 export const font = (size: number) =>
 	`${size}px 'Manrope', system-ui, 'SF Pro Display', Roboto, Oxygen, Arial, sans-serif`
+
+export const INIT_SLIDES: Slide[] = [{ id: '1', elements: [] }]
